a43437e5fe7106afe6e4070fff3be8673353e872
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2024-09-26 21:56:10 +0530

    setup login

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,9 @@ import "../src/css/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 reportWebVitals();
