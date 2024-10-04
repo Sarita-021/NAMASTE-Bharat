bd5754a2ba9e0dbf53bbf5a45885dc76eb5c828a
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2024-10-04 15:38:15 +0530

    change the name of travellers

--- client/src/pages/Traveller/purchasehistory.js
+++ client/src/pages/Traveller/purchasehistory.js
@@ -2,11 +2,11 @@ import react, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 
 const Purchase_History = () => {
-    return (
-        <header>
-            <div id="navbar">Purchase_History</div>
-        </header>
-    );
-}
+  return (
+    <header>
+      <div id="navbar">Purchase_History</div>
+    </header>
+  );
+};
 
 export default Purchase_History;
